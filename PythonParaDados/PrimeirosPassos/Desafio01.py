4e8908e7761855cb2c77bb17d7462480a6c123e4
Vitor Almeida Machado <116811472+vitorvitoor@users.noreply.github.com>
2025-12-09 15:49:40 -0300

    Add files via upload

---
+++ PythonParaDados/PrimeirosPassos/Desafio01.py
@@ -0,0 +1,19 @@
+#Imprima a frase Escola de Dados da Alura!.
+print("Escola de Dados da Alura")
+
+#Imprima seu nome e seu sobrenome seguindo a estrutura abaixo:
+print("Vitor")
+print("Almeida Machado")
+
+#Imprima o seu primeiro nome letra a letra.
+print("V")
+print("I")
+print("T")
+print("O")
+print("R")
+
+#Imprima o dia do seu nascimento em formato dia mês ano. Lembrando que os valores de dia e ano não podem estar entre aspas. Supondo uma data de aniversário dia 28 de fevereiro de 2003, o formato deve estar como no exemplo abaixo:
+print(24, "Agosto", 2001)
+
+#Imprima, em um único print, o atual ano que você está fazendo esse curso. O valor do ano deve ser um dado numérico e a saída do print deve ser a seguinte:
+print("Ano atual:", 2025)
